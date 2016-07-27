a44b28af330829f03546d1cb6edbba232ad9abb8
sjhogan2 <shannon.jane.hogan@gmail.com>
2016-07-27 22:33:29 +0000

    Add sold out badge feature to products listing

--- db/seeds.rb
+++ db/seeds.rb
@@ -36,7 +36,7 @@ cat1.products.create!({
   name:  'Men\'s Classy shirt',
   description: Faker::Hipster.paragraph(4),
   image: open_asset('apparel1.jpg'),
-  quantity: 10,
+  quantity: 0,
   price: 64.99
 })
 
