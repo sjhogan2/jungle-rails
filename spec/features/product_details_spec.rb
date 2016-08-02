906293a5589721102f4aba755293afd7f6310fde
sjhogan2 <shannon.jane.hogan@gmail.com>
2016-08-02 18:55:34 +0000

    Generate product details feature spec

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor navigates to product details", type: :feature, js: true do
+
+ # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        quantity: 10,
+        image: open_asset('apparel1.jpg'),
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They see that product's details" do
+    # ACT
+    visit root_path
+    find('').click
+
+    # DEBUG / VERIFY
+    save_screenshot
+    expect(page).to have_css 'article.product', count: 10
+  end
+
+end
